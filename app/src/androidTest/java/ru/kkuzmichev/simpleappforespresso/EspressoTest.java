e20d1a9662fdcd002c3ec4f90ed21f536ff2754b
unknown <orlovdimas92@mail.ru>
2022-01-22 00:53:47 +0300

    Zadanie 1. 2.6

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTest.java
@@ -1,11 +1,19 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Intent;
+
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -29,5 +37,28 @@ public class EspressoTest {
                         withText("This is home fragment")
                 )
         );
+    }
+
+    @Test
+    public void testIntent() {
+        ViewInteraction element = onView(
+                withContentDescription("More options")
+        );
+        element.perform(click());
+        ViewInteraction elementText = onView(
+                withId(R.id.title)
+        );
+        elementText.check(
+                matches(
+                        withText("Settings")
+                )
+        );
+        Intents.init();
+        elementText.perform(click());
+        //Проверяем intent, он должен передавать url и action
+        //Intents..
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
     }
 }
